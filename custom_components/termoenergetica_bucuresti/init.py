83e1371bdc9b8b54f05dfb196bc714424baefcb7
George <enageo20@gmail.com>
2025-11-04 13:55:03 +0200

    Update init.py

--- custom_components/termoenergetica_bucuresti/init.py
+++ custom_components/termoenergetica_bucuresti/init.py
@@ -4,21 +4,17 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
-
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Setare configurare."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.data.setdefault("termoenergetica_bucuresti", {})
     
+    # Setup platform sensor
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Încărcare configurare."""
-    unload_ok = await hass.config_entries.async_unload_entries(entry, ["sensor"])
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    return True
