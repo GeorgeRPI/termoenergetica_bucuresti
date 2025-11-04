e8c8fece23025c550b59c6c193aada51c2b0a3e8
George <enageo20@gmail.com>
2025-11-04 15:50:49 +0200

    Update and rename init.py to __init__.py

--- config/custom_components/termoenergetica_bucuresti/__init__.py
+++ config/custom_components/termoenergetica_bucuresti/__init__.py
@@ -1,20 +1,12 @@
-"""Platforma Termoenergetica București."""
-from __future__ import annotations
-import logging
+"""Termoenergetica Bucuresti."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-_LOGGER = logging.getLogger(__name__)
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Setare configurare."""
-    hass.data.setdefault("termoenergetica_bucuresti", {})
-    
-    # Setup platform sensor
+    """Set up from config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Încărcare configurare."""
-    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    """Unload config entry."""
     return True
